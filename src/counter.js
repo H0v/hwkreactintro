9aeb144c0f6ac33e95edc66d7d8c58b6028ce499
Hovhannes <tonoyan6666@gmail.com>
2020-01-23 23:29:09 +0400

    all project

---
+++ src/counter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            counter : +localStorage.getItem("counter")
+         }
+    }
+
+    increment(){
+        localStorage.setItem("counter",String(this.state.counter + 1));
+            this.setState(state => ({
+            counter : +localStorage.getItem("counter")
+        }));
+    }
+    decrement(){
+        if( this.state.counter !== 0){
+            localStorage.setItem("counter",String(this.state.counter + -1));
+            this.setState(state => ({
+               counter : +localStorage.getItem("counter")
+            }));
+        }
+
+    }
+    reset(){
+        localStorage.setItem("counter","0");
+        this.setState(state => ({
+            counter : +localStorage.getItem("counter")
+         }));
+    }
+    render() { 
+        return ( 
+            <>
+            <button onClick={() => this.increment()}>Increment</button>
+            <span>{this.state.counter}</span>
+            <button onClick={() => this.decrement()}>Decrement</button>
+            <button onClick={() => this.reset()}>Reset</button>
+            </>
+            );
+    }
+}
+ 
+export default Counter;
